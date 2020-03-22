8e2dd10356dc70722b1691b72bda36214c057106
FFreshman <1366157248@qq.com>
2020-03-22 23:08:47 +0800

    完成group-button

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './Button.vue'
 import Icon from './Icon.vue'
+import ButtonGroup from './Button-group.vue'
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
+Vue.component('g-button-group',ButtonGroup)
 new Vue({
   data:{
     isLoading:false
