10fcfd72ab53fde20f37ba73e99587f5cf931cde
FFreshman <1366157248@qq.com>
2020-03-23 00:21:01 +0800

    使用chai-spy监听回调函数

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ new Vue({
 })
 //单元测试
 import chai from 'chai'
-
+import spies from  'chai-spies'
+chai.use(spies)
 const expect = chai.expect
 {
   const Constructor = Vue.extend(Button)
@@ -87,11 +88,11 @@ const expect = chai.expect
     }
   })
   vm.$mount()
-  vm.$on('click', function () {
-    expect(1).to.eq(1)
-  })
+  let spy=chai.spy(function(){})
+  vm.$on('click',spy)
   // 希望这个函数被执行
   let button = vm.$el
   button.click()
+  expect(spy).to.have.been.called()
 }
 
