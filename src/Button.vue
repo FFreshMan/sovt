dafdf63f065f30c3bd456df2a545018af23f5a4b
FFreshman <1366157248@qq.com>
2020-03-22 21:55:12 +0800

    将svg封装到Icon组件中，供全局调用

--- src/Button.vue
+++ src/Button.vue
@@ -1,10 +1,24 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <g-icon v-if="icon" :name="icon"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script lang="js">
-  export default
-  {
+  export default {
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value){
+          return !(value !== 'left' && value !== 'right');
+        }
+      }
+    }
   }
 </script>
 
@@ -16,6 +30,21 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    //不加这句会对不齐
+    vertical-align: middle;
+
+    > .icon {
+      margin-right: .2em;
+      order: 1;
+    }
+
+    > .content {
+      order: 2;
+    }
+
     &:hover {
       border-color: var(--border-color-hover);
     }
@@ -26,6 +55,18 @@
 
     &:focus {
       outline: none;
+    }
+
+    &.icon-right {
+      > .icon {
+        margin-left: .2em;
+        margin-right: 0;
+        order: 2;
+      }
+
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
