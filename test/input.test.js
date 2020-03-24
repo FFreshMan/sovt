b44cb7406f70dfaa32fbd54376ea5711cad2a3d1
FFreshman <1366157248@qq.com>
2020-03-24 17:08:19 +0800

    为Input组件添加了一个onchange事件的测试，测试通过

--- test/input.test.js
+++ test/input.test.js
@@ -7,16 +7,19 @@ import Input from '../src/Input'
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
+describe('input', () => {
+  expect(Input).to.exist
+})
 describe('props', () => {
   const Constructor = Vue.extend(Input)
   let vm
-  afterEach(()=>{
+  afterEach(() => {
     vm.$destroy()
   })
   it('可以传value', () => {
     vm = new Constructor({
       propsData: {
-        value:'123'
+        value: '123'
       }
     }).$mount()
     const inputElement = vm.$el.querySelector('Input')
@@ -25,7 +28,7 @@ describe('props', () => {
   it('传disabled', () => {
     vm = new Constructor({
       propsData: {
-        disabled:true
+        disabled: true
       }
     }).$mount()
     const inputElement = vm.$el.querySelector('Input')
@@ -34,7 +37,7 @@ describe('props', () => {
   it('传readonly', () => {
     vm = new Constructor({
       propsData: {
-       readonly:true
+        readonly: true
       }
     }).$mount()
     const inputElement = vm.$el.querySelector('Input')
@@ -43,7 +46,7 @@ describe('props', () => {
   it('传error', () => {
     vm = new Constructor({
       propsData: {
-        error:'测试error'
+        error: '测试error'
       }
     }).$mount()
     const spanElement = vm.$el.querySelector('span')
@@ -52,14 +55,26 @@ describe('props', () => {
   it('Icon是warning', () => {
     vm = new Constructor({
       propsData: {
-        error:'测试error'
+        error: '测试error'
       }
     }).$mount()
     const iconElement = vm.$el.querySelector('use')
     expect(iconElement.getAttribute('xlink:href')).to.equal('#warning')
   })
 })
-// describe('事件',()=>{
-//   it('支持change', ()=>{
-//   })
-// })
+describe('事件',()=>{
+  const Constructor = Vue.extend(Input)
+  let vm
+  afterEach(() => {
+    vm.$destroy()
+  })
+  it('支持change', ()=>{
+    vm = new Constructor({}).$mount()
+    const callback=sinon.fake()
+    vm.$on('change',callback)
+    let event=new Event('change')
+    let inputElement=vm.$el.querySelector('input')
+    inputElement.dispatchEvent(event)
+    expect(callback).to.have.been.called
+  })
+})
