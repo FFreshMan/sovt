99ce9230daac7c96c60d73504674cdfb02833e30
FFreshman <1366157248@qq.com>
2020-03-26 15:06:50 +0800

    完成toast组件基本功能，callback

--- src/Toast.vue
+++ src/Toast.vue
@@ -2,28 +2,82 @@
   <div class="toast">
     <slot>
     </slot>
+    <div class="line"></div>
+    <span v-if="closeButton" class="close" @click="onClickClose">
+      {{closeButton.text}}
+    </span>
   </div>
 </template>
 
 <script lang="ts">
   export default {
-    name: 'g-toast'
+    name: 'g-toast',
+    props: {
+      autoClose: {
+        type: Boolean,
+        default: true
+      },
+      autoCloseDelay: {
+        type: Number,
+        default: 50
+      },
+      closeButton: {
+        //props若是对象则需用函数返回
+        type: Object,
+        default: () => {
+          return {
+            text: '关闭', callback: (toast) => {
+              toast.close();
+            }
+          };
+        }
+      }
+    },
+    mounted() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoCloseDelay * 1000);
+      }
+    },
+    methods: {
+      close() {
+        this.$el.remove();
+        this.$destroy();
+      },
+      onClickClose() {
+        this.closeButton.callback(this);
+        this.close();
+      }
+    }
   };
 </script>
 
 <style lang="scss" scoped>
   $font-size: 14px;
   $toast-height: 40px;
-  $toast-bg:rgba(0,0,0,0.75);
-  $toast-shadow:0px 0px 3px 0px rgba(0,0,0,0.5);
+  $toast-bg: rgba(0, 0, 0, 0.75);
+  $toast-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
   .toast {
+    border-radius: 5px;
     height: $toast-height;
     position: fixed;left: 50%;
     top: 0;transform: translateX(-50%);font-size: $font-size;
     line-height: 1.8;display: flex;align-items: center;
     background: $toast-bg;
-    box-shadow:$toast-shadow;
+    box-shadow: $toast-shadow;
     color: #fff;
     padding: 0 16px;
   }
+
+  .close {
+    padding-left: 16px;
+  }
+
+  .line {
+    border-left: 2px solid #666;
+    margin-left: 16px;
+    height: 100%;
+  }
+
 </style>
--- src/plugin.js
+++ src/plugin.js
@@ -1,13 +1,22 @@
 import Toast from './Toast'
+
 export default {
-    install(Vue,options){
-      Vue.prototype.$toast=function (message) {
-        const Constructor=Vue.extend(Toast)
-        const toast=new Constructor()
-        //先插入再mount
-        toast.$slots.default=message
-        toast.$mount()
-        document.body.appendChild(toast.$el)
-      }
+  install(Vue, options) {
+    Vue.prototype.$toast = function (message) {
+      const Constructor = Vue.extend(Toast)
+      const toast = new Constructor({
+        propsData: {
+          closeButton: {
+            text: '测试',
+            callback(){
+            }
+          }
+        }
+      })
+      //先插入再mount
+      toast.$slots.default = message
+      toast.$mount()
+      document.body.appendChild(toast.$el)
     }
+  }
 }
