122ee2d2f13cb0db4f09fd3a2b1dd9ae33172d0f
FFreshman <1366157248@qq.com>
2020-03-26 16:40:13 +0800

    实现只显示一个toast功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,26 @@
 import Toast from './Toast'
 
+
+let currentToast
 export default {
   install(Vue, options) {
     Vue.prototype.$toast = function (message,toastOptions) {
-      const Constructor = Vue.extend(Toast)
-      const toast = new Constructor({
-        propsData: toastOptions
-      })
-      //先插入再mount
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+      if(currentToast){
+        currentToast.close()
+      }
+      currentToast=createToast({Vue,message,propsData:toastOptions})
     }
   }
+}
+
+function createToast({Vue,message,propsData}){
+  const Constructor = Vue.extend(Toast)
+  const toast = new Constructor({
+    propsData: propsData
+  })
+  //先插入再mount
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
 }
