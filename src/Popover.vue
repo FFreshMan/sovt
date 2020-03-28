551ca6425bd23deca02d89e97356f4e226130e9a
FFreshman <1366157248@qq.com>
2020-03-28 18:50:24 +0800

    表驱动编程重构

--- src/Popover.vue
+++ src/Popover.vue
@@ -36,27 +36,25 @@
     //@click.stop阻止冒泡
     methods: {
       positionContent() {
-        document.body.appendChild(this.$refs.contentWrapper);
-        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
-        const contentWrapper = this.$refs.contentWrapper;
-        if (this.position === 'top') {
-          contentWrapper.style.left = left + window.scrollX + 'px';
-          contentWrapper.style.top = top + window.scrollY + 'px';
-        } else if (this.position === 'bottom') {
-          contentWrapper.style.left = left + window.scrollX + 'px';
-          contentWrapper.style.top = top + height + window.scrollY + 'px';
-        } else if (this.position === 'left') {
-          contentWrapper.style.left = left + window.scrollX + 'px';
-          let {height: height2} = contentWrapper.getBoundingClientRect();
-          contentWrapper.style.top = top + window.scrollY +
-            (height - height2) / 2 + 'px';
-        } else if (this.position === 'right') {
-          contentWrapper.style.left = left + window.scrollX + width + 'px';
-          let {height: height2} = contentWrapper.getBoundingClientRect();
-          contentWrapper.style.top = top + window.scrollY +
-            (height - height2) / 2 + 'px';
-        }
-        this.$refs.contentWrapper.style.wordBreak = 'break-all';
+        const {contentWrapper, triggerWrapper} = this.$refs;
+        document.body.appendChild(contentWrapper);
+        const {width, height, top, left} = triggerWrapper.getBoundingClientRect();
+        const {height: height2} = contentWrapper.getBoundingClientRect();
+        let positions = {
+          top: {top: top + window.scrollY, left: left + window.scrollX,},
+          bottom: {top: top + height + window.scrollY, left: left + window.scrollX},
+          left: {
+            top: top + window.scrollY + (height - height2) / 2,
+            left: left + window.scrollX
+          },
+          right: {
+            top: top + window.scrollY + (height - height2) / 2,
+            left: left + window.scrollX + width
+          },
+        };
+        contentWrapper.style.left = positions[this.position].left + 'px';
+        contentWrapper.style.top = positions[this.position].top + 'px';
+        contentWrapper.style.wordBreak = 'break-all';
       },
       onClickDocument(e) {
         if (this.$refs.popover &&
