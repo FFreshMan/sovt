7a489f42b1c31fdb67c2a807c4b23720ca4fc1a1
FFreshman <1366157248@qq.com>
2020-03-28 17:13:50 +0800

    优化popover样式，添加小三角和阴影

--- src/Popover.vue
+++ src/Popover.vue
@@ -29,11 +29,16 @@
         let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
         this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
         this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+        this.$refs.contentWrapper.style.wordBreak = 'break-all';
       },
       onClickDocument(e) {
         if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
-        ) { return }
+        ) { return; }
+        if (this.$refs.contentWrapper &&
+          this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target)) {
+          return;
+        }
         this.close();
       },
       openPop() {
@@ -62,18 +67,45 @@
 
 <style lang="scss" scoped>
   $box-shadow-color: 0 0 3px rgba(0, 0, 0, 0.5);
+  $border-color: #333;
+  $border-radius: 4px;
   .popover {
     display: inline-block;
     vertical-align: top;
-    border: 1px solid;
     position: relative;
-    margin: 30px 50px;
+    margin: 300px 500px;
   }
 
   .content-wrapper {
     position: absolute;
-    left: 0;
-    box-shadow: $box-shadow-color;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+    background: white;
     transform: translateY(-100%);
+    margin-top: -10px;
+    padding: .5em 1em;
+    max-width: 20em;
+
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 10px;
+    }
+
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
   }
 </style>
