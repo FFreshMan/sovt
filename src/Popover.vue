6f4da98d1ee5feb5c1f185bec44b190edbfe9bf8
FFreshman <1366157248@qq.com>
2020-03-28 16:39:15 +0800

    基本完成popover

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content">
       </slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block;">
     <slot>
     </slot>
     </span>
@@ -22,25 +22,38 @@
     mounted() {
 
     },
+    //@click.stop阻止冒泡
     methods: {
-      //@click.stop阻止冒泡
-      xxx() {
-        this.visible = !this.visible;
-        if (this.visible === true) {
-          this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper);
-            // getBoundingClientRect是可视区域，absolute相对body
-            let {width,height,top,left}=this.$refs.triggerWrapper.getBoundingClientRect();
-            this.$refs.contentWrapper.style.left=`${left+window.scrollX}px`;
-            this.$refs.contentWrapper.style.top=`${top+window.scrollY}px`;
-            console.log(this.$refs.contentWrapper.style.top);
-            let eventHandler = () => {
-              this.visible = false;
-              document.removeEventListener('click', eventHandler);
-            };
-            //x.bind(this)会变成一个新函数对象
-            document.addEventListener('click', eventHandler);
-          });
+      positionContent() {
+        document.body.appendChild(this.$refs.contentWrapper);
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+      },
+      onClickDocument(e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        this.close();
+      },
+      openPop() {
+        this.visible = true;
+        setTimeout(() => {
+          this.positionContent();
+          document.addEventListener('click', this.onClickDocument);
+        });
+      },
+      close() {
+        this.visible = false;
+        document.removeEventListener('click', this.onClickDocument);
+      },
+      onClick(event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close();
+          } else {
+            this.openPop();
+          }
         }
       }
     }
@@ -56,10 +69,11 @@
     position: relative;
     margin: 30px 50px;
   }
+
   .content-wrapper {
     position: absolute;
     left: 0;
     box-shadow: $box-shadow-color;
     transform: translateY(-100%);
-    }
+  }
 </style>
